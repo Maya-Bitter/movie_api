cfd58116989b8a2ba960fa027e824a85b632c851
Maya-Bitter <mayabitter@gmail.com>
2023-03-31 09:11:05 +0200

    install bcrypt

--- index.js
+++ index.js
@@ -22,6 +22,21 @@ app.use(morgan("common"));
 const cors = require('cors');
 app.use(cors());
 
+// By default, it will set the application to allow requests from all origins; however, if you want only certain origins to be given access, you’ll need to replace app.use(cors()); with the following code:
+
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+//  app.use(cors({
+//  origin: (origin, callback) => {
+//  if(!origin) return callback(null, true);
+//  if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//  let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//  return callback(new Error(message ), false);
+//  }
+//  return callback(null, true);
+//  }
+//  }));
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
