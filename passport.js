58db7e92876126d7cd5889f2001ebd12f001b395
Maya-Bitter <mayabitter@gmail.com>
2023-04-06 15:22:24 +0200

    .

--- passport.js
+++ passport.js
@@ -7,51 +7,21 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-  // New code:
-
-  passport.use(new LocalStrategy({
-    usernameField: 'Username',
-    passwordField: 'Password'
-  }, (username, password, callback) => {
-    console.log(username + '  ' + password);
-    Users.findOne({ Username: username }, (error, user) => {
-      if (error) {
-        console.log(error);
-        return callback(error);
-      }
-  
-      if (!user) {
-        console.log('incorrect username');
-        return callback(null, false, {message: 'Incorrect username.'});
-      }
-  
-      if (!user.validatePassword(password)) {
-        console.log('incorrect password');
-        return callback(null, false, {message: 'Incorrect password.'});
-      }
-  
-      console.log('finished');
-      return callback(null, user);
-    });
-  }));
-
-  // Old code 
-
-//passport.use(new LocalStrategy({
-//usernameField: 'Username',
-//passwordField: 'Password'  
-//}, (username, password, callback) => {
-//console.log(username + ' ' + password);
-//Users.findOne({Username: username})
-//.then((user) => {
-//  if (!user) {
-//      console.log('Incorrect username!');
-//      return callback(null, false, {message: 'Incorrect username or password!'});
-// }
-//  console.log('Finished!');
-//  return callback(null, user)
-//})
-//}));
+passport.use(new LocalStrategy({
+usernameField: 'Username',
+passwordField: 'Password'  
+}, (username, password, callback) => {
+console.log(username + ' ' + password);
+Users.findOne({Username: username})
+.then((user) => {
+  if (!user) {
+      console.log('Incorrect username!');
+      return callback(null, false, {message: 'Incorrect username or password!'});
+  }
+  console.log('Finished!');
+  return callback(null, user)
+})
+}));
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
