652913a20459197c07ae73d47cc95ce284d72c63
Maya-Bitter <mayabitter@gmail.com>
2023-07-07 15:07:54 +0200

    added comments

--- auth.js
+++ auth.js
@@ -1,27 +1,42 @@
-const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+const jwtSecret = "your_jwt_secret"; // This has to be the same key used in the JWTStrategy
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
 
-require('./passport'); // Your local passport file
+require("./passport");
 
+/**
+ *
+ * @param {*} user
+ * @returns
+ * This function takes the user object and creates a JWT using the sign() method.
+ * The first argument is the user object to encode into the JWT.
+ *
+ */
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username you’re encoding in the JWT
-    expiresIn: '7d', // This specifies that the token will expire in 7 days
-    algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+    expiresIn: "7d", // This specifies that the token will expire in 7 days
+    algorithm: "HS256", // This is the algorithm used to “sign” or encode the values of the JWT
   });
-}
+};
 
+/**
+ *
+ * @param {*} router
+ * This function is responsible for authenticating users.
+ * It uses the passport.authenticate() method, passing in the local strategy and a callback function.
+ */
 /* POST login. */
+
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+          message: "Something is not right",
+          user: user,
         });
       }
       req.login(user, { session: false }, (error) => {
@@ -33,4 +48,4 @@ module.exports = (router) => {
       });
     })(req, res);
   });
-}
+};
